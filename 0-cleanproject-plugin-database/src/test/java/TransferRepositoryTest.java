1f72f521025b882d9ccf34ee24e02c7a29bce3d4
MeNoSmartBrain <ya.hemmer@gmail.com>
2022-04-05 19:14:09 +0200

    Restructure tests for TransferRepository

--- 0-cleanproject-plugin-database/src/test/java/TransferRepositoryTest.java
+++ 0-cleanproject-plugin-database/src/test/java/TransferRepositoryTest.java
@@ -1,4 +1,5 @@
 import IO.CSVreader;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.MockedStatic;
 import org.mockito.Mockito;
@@ -10,15 +11,22 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class TransferRepositoryTest {
 
+    private LinkedList<String> transfers;
+    private UUID querryId;
+    private Transfer foundTransfer;
+
+    @BeforeEach
+    void init(){
+        this.transfers = new LinkedList<String>();
+        this.foundTransfer = null;
+    }
+
     @Test
     void getFirst() throws InvalidAmountException {
-        LinkedList<String> transfers = new LinkedList<String>();
+        this.transfers.add("5267debe-af38-11ec-b909-0242ac120002;223e4567-e89b-12d3-a456-000000000000;223e4567-e89b-12d3-a456-222222222222;0.1;;1648118619\n");
+        this.transfers.add("5267e1c0-af38-11ec-b909-0242ac120002;223e4567-e89b-12d3-a456-222222222222;07a3d2d5-95c3-4ef9-8c5d-03f32e3488f6;1.2;0;1648305732\n");
 
-        transfers.add("5267debe-af38-11ec-b909-0242ac120002;223e4567-e89b-12d3-a456-000000000000;223e4567-e89b-12d3-a456-222222222222;0.1;;1648118619\n");
-        transfers.add("5267e1c0-af38-11ec-b909-0242ac120002;223e4567-e89b-12d3-a456-222222222222;07a3d2d5-95c3-4ef9-8c5d-03f32e3488f6;1.2;0;1648305732\n");
-
-        UUID querryId = UUID.fromString("5267debe-af38-11ec-b909-0242ac120002");
-        Transfer foundTransfer = null;
+        this.querryId = UUID.fromString("5267debe-af38-11ec-b909-0242ac120002");
         try (MockedStatic<CSVreader> mockPersistence = Mockito.mockStatic(CSVreader.class)) {
             mockPersistence.when(() -> CSVreader.read(TransferRepository.TRANSFER_FILEPATH, "\r\n")).thenReturn(transfers);
 
@@ -32,13 +40,10 @@ class TransferRepositoryTest {
 
     @Test
     void getSecond() throws InvalidAmountException {
-        LinkedList<String> transfers = new LinkedList<String>();
+        this.transfers.add("5267debe-af38-11ec-b909-0242ac120002;223e4567-e89b-12d3-a456-000000000000;223e4567-e89b-12d3-a456-222222222222;0.1;;1648118619\n");
+        this.transfers.add("5267e1c0-af38-11ec-b909-0242ac120002;223e4567-e89b-12d3-a456-222222222222;07a3d2d5-95c3-4ef9-8c5d-03f32e3488f6;1.2;;1648305732\n");
 
-        transfers.add("5267debe-af38-11ec-b909-0242ac120002;223e4567-e89b-12d3-a456-000000000000;223e4567-e89b-12d3-a456-222222222222;0.1;;1648118619\n");
-        transfers.add("5267e1c0-af38-11ec-b909-0242ac120002;223e4567-e89b-12d3-a456-222222222222;07a3d2d5-95c3-4ef9-8c5d-03f32e3488f6;1.2;;1648305732\n");
-
-        UUID querryId = UUID.fromString("5267e1c0-af38-11ec-b909-0242ac120002");
-        Transfer foundTransfer = null;
+        this.querryId = UUID.fromString("5267e1c0-af38-11ec-b909-0242ac120002");
         try (MockedStatic<CSVreader> mockPersistence = Mockito.mockStatic(CSVreader.class)) {
             mockPersistence.when(() -> CSVreader.read(TransferRepository.TRANSFER_FILEPATH, "\r\n")).thenReturn(transfers);
 
@@ -52,13 +57,10 @@ class TransferRepositoryTest {
 
     @Test
     void searchIdThatDoesNotExists() {
-        LinkedList<String> transfers = new LinkedList<String>();
+        this.transfers.add("5267debe-af38-11ec-b909-0242ac120002;223e4567-e89b-12d3-a456-000000000000;223e4567-e89b-12d3-a456-222222222222;0.1;;1648118619\n");
+        this.transfers.add("5267e1c0-af38-11ec-b909-0242ac120002;223e4567-e89b-12d3-a456-222222222222;07a3d2d5-95c3-4ef9-8c5d-03f32e3488f6;1.2;0;1648305732\n");
 
-        transfers.add("5267debe-af38-11ec-b909-0242ac120002;223e4567-e89b-12d3-a456-000000000000;223e4567-e89b-12d3-a456-222222222222;0.1;;1648118619\n");
-        transfers.add("5267e1c0-af38-11ec-b909-0242ac120002;223e4567-e89b-12d3-a456-222222222222;07a3d2d5-95c3-4ef9-8c5d-03f32e3488f6;1.2;0;1648305732\n");
-
-        UUID querryId = UUID.fromString("A267debe-af38-11ec-b909-0242ac120002");
-        Transfer foundTransfer = null;
+        this.querryId = UUID.fromString("A267debe-af38-11ec-b909-0242ac120002");
         try (MockedStatic<CSVreader> mockPersistence = Mockito.mockStatic(CSVreader.class)) {
             mockPersistence.when(() -> CSVreader.read(TransferRepository.TRANSFER_FILEPATH, "\r\n")).thenReturn(transfers);
 
