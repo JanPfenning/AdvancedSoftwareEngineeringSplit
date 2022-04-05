1c923ff83dcf22665e80768a1b4f44ed623ec46d
MeNoSmartBrain <ya.hemmer@gmail.com>
2022-04-05 15:10:51 +0200

    Add test for TransferSerice

--- 2-cleanproject-application/src/test/java/TransferServiceTest.java
+++ 2-cleanproject-application/src/test/java/TransferServiceTest.java
@@ -1,3 +1,4 @@
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import java.util.UUID;
@@ -7,55 +8,118 @@ import static org.mockito.Mockito.*;
 
 class TransferServiceTest {
 
+    private static UUID moneypoolIDofSender = UUID.fromString("223e4567-e89b-12d3-a456-000000000000");
+    private static UUID moneypoolIDofReciever = UUID.fromString("223e4567-e89b-12d3-a456-000000000000");
+
+    private static UUID accountIDofSender = UUID.fromString("123e4567-e89b-12d3-a456-111111111111");
+    private static UUID accountIDofReciever = UUID.fromString("123e4567-e89b-12d3-a456-222222222222");
+
+
+    private TransferRepositoryInterface mockedTransferRepository;
+    private UserRepositoryInterface mockedUserRepository;
+
+    private UserAggregate mockedSender;
+    private UserAggregate mockedReceiver;
+
+    @BeforeAll
+    void init(){
+        this.mockedTransferRepository = mock(TransferRepositoryInterface.class);
+        this.mockedUserRepository = mock(UserRepositoryInterface.class);
+
+        this.mockedSender = mock(UserAggregate.class);
+        this.mockedReceiver = mock(UserAggregate.class);
+    }
+
+
     @Test
     void senderInsufficientMoney() throws Exception {
-        TransferRepositoryInterface mockedTransferRepository = mock(TransferRepositoryInterface.class);
-        UserRepositoryInterface mockedUserRepository = mock(UserRepositoryInterface.class);
         Depot mockedSenderDepot = mock(Account.class);
         Depot mockedReceiverDepot = mock(Account.class);
-        UserAggregate mockedSender = mock(UserAggregate.class);
-        UserAggregate mockedReceiver = mock(UserAggregate.class);
 
-        when(mockedUserRepository.getUserFrom(UUID.fromString("123e4567-e89b-12d3-a456-111111111111"))).thenReturn(mockedSender);
-        when(mockedUserRepository.getUserFrom(UUID.fromString("123e4567-e89b-12d3-a456-222222222222"))).thenReturn(mockedReceiver);
+        when(mockedUserRepository.getUserFrom(accountIDofSender)).thenReturn(mockedSender);
+        when(mockedUserRepository.getUserFrom(accountIDofReciever)).thenReturn(mockedReceiver);
 
-        when(mockedSender.getDepotBy(UUID.fromString("123e4567-e89b-12d3-a456-111111111111"))).thenReturn(mockedSenderDepot);
-        when(mockedReceiver.getDepotBy(UUID.fromString("123e4567-e89b-12d3-a456-222222222222"))).thenReturn(mockedSenderDepot);
+        when(mockedSender.getDepotBy(accountIDofSender)).thenReturn(mockedSenderDepot);
+        when(mockedReceiver.getDepotBy(accountIDofReciever)).thenReturn(mockedSenderDepot);
 
         when(mockedSenderDepot.getBalance()).thenReturn(new Balance(0));
         when(mockedReceiverDepot.getBalance()).thenReturn(new Balance(0));
 
         TransferService service = new TransferService(mockedUserRepository, mockedTransferRepository);
         assertThrows(InvalidBalanceException.class, ()->{
-            service.sendMoney("123e4567-e89b-12d3-a456-111111111111", "123e4567-e89b-12d3-a456-222222222222", new Amount(1));
+            service.sendMoney(accountIDofSender.toString(), accountIDofReciever.toString(), new Amount(1));
         });
     }
 
     @Test
     void AccountToAccountHappyPath() throws Exception {
-        TransferRepositoryInterface mockedTransferRepository = mock(TransferRepositoryInterface.class);
-        UserRepositoryInterface mockedUserRepository = mock(UserRepositoryInterface.class);
-        UserAggregate mockedSender = mock(UserAggregate.class);
-        UserAggregate mockedReceiver = mock(UserAggregate.class);
         Depot mockedSenderDepot = mock(Account.class);
         Depot mockedReceiverDepot = mock(Account.class);
 
-        when(mockedUserRepository.getUserFrom(UUID.fromString("123e4567-e89b-12d3-a456-111111111111"))).thenReturn(mockedSender);
-        when(mockedUserRepository.getUserFrom(UUID.fromString("123e4567-e89b-12d3-a456-222222222222"))).thenReturn(mockedReceiver);
+        when(mockedUserRepository.getUserFrom(moneypoolIDofSender)).thenReturn(mockedSender);
+        when(mockedUserRepository.getUserFrom(moneypoolIDofReciever)).thenReturn(mockedReceiver);
 
-        when(mockedSender.getDepotBy(UUID.fromString("123e4567-e89b-12d3-a456-111111111111"))).thenReturn(mockedSenderDepot);
-        when(mockedReceiver.getDepotBy(UUID.fromString("123e4567-e89b-12d3-a456-222222222222"))).thenReturn(mockedReceiverDepot);
+        when(mockedSender.getDepotBy(moneypoolIDofSender)).thenReturn(mockedSenderDepot);
+        when(mockedReceiver.getDepotBy(moneypoolIDofReciever)).thenReturn(mockedReceiverDepot);
 
         when(mockedSenderDepot.getBalance()).thenReturn(new Balance(2));
         when(mockedReceiverDepot.getBalance()).thenReturn(new Balance(0));
 
         TransferService service = new TransferService(mockedUserRepository, mockedTransferRepository);
         assertDoesNotThrow(()->{
-            service.sendMoney("123e4567-e89b-12d3-a456-111111111111", "123e4567-e89b-12d3-a456-222222222222", new Amount(1));
+            service.sendMoney(moneypoolIDofSender.toString(), moneypoolIDofReciever.toString(), new Amount(1));
         });
     }
 
-    //TODO test send from moneypool to moneypool
-    //TODO test send from Moneypool to account
-    //TODO test send from Account to moneypool
+    @Test
+    void moneypoolToMoneypool() {
+        Depot mockedSenderDepot = mock(Moneypool.class);
+        Depot mockedReceiverDepot = mock(Moneypool.class);
+
+        when(mockedUserRepository.getUserFrom(moneypoolIDofSender)).thenReturn(mockedSender);
+        when(mockedUserRepository.getUserFrom(moneypoolIDofReciever)).thenReturn(mockedReceiver);
+
+        when(mockedSender.getDepotBy(moneypoolIDofSender)).thenReturn(mockedSenderDepot);
+        when(mockedReceiver.getDepotBy(moneypoolIDofReciever)).thenReturn(mockedReceiverDepot);
+
+        TransferService service = new TransferService(mockedUserRepository, mockedTransferRepository);
+        assertThrows( TransferOutOfMoneypoolException.class,() ->{
+            service.sendMoney(moneypoolIDofSender.toString(), moneypoolIDofReciever.toString(), new Amount(1));
+        });
+    }
+
+    @Test
+    void moneypoolToAccount() {
+        Depot mockedSenderDepot = mock(Moneypool.class);
+        Depot mockedReceiverDepot = mock(Account.class);
+
+        when(mockedUserRepository.getUserFrom(moneypoolIDofSender)).thenReturn(mockedSender);
+        when(mockedUserRepository.getUserFrom(accountIDofReciever)).thenReturn(mockedReceiver);
+
+        when(mockedSender.getDepotBy(moneypoolIDofSender)).thenReturn(mockedSenderDepot);
+        when(mockedReceiver.getDepotBy(accountIDofReciever)).thenReturn(mockedReceiverDepot);
+
+        TransferService service = new TransferService(mockedUserRepository, mockedTransferRepository);
+        assertThrows( TransferOutOfMoneypoolException.class,() ->{
+            service.sendMoney(moneypoolIDofSender.toString(), accountIDofReciever.toString(), new Amount(1));
+        });
+    }
+
+    @Test
+    void accountToMoneypool() throws Exception{
+        Depot mockedSenderDepot = mock(Account.class);
+        Depot mockedReceiverDepot = mock(Moneypool.class);
+
+        when(mockedUserRepository.getUserFrom(accountIDofSender)).thenReturn(mockedSender);
+        when(mockedUserRepository.getUserFrom(moneypoolIDofReciever)).thenReturn(mockedReceiver);
+
+        when(mockedSender.getDepotBy(accountIDofSender)).thenReturn(mockedSenderDepot);
+        when(mockedReceiver.getDepotBy(moneypoolIDofReciever)).thenReturn(mockedReceiverDepot);
+
+        TransferService service = new TransferService(mockedUserRepository, mockedTransferRepository);
+        assertDoesNotThrow(() ->{
+            service.sendMoney(accountIDofSender.toString(), moneypoolIDofReciever.toString(), new Amount(1));
+        });
+    }
+
 }
