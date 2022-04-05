9ec0edd9e7d808927b6f8db0d4cdc43e6f86e6d0
Jan Pfenning <jan.pfenning@gmail.com>
2022-04-05 14:59:07 +0200

    Update Depot.java

--- 3-cleanproject-domain/src/main/java/Depot.java
+++ 3-cleanproject-domain/src/main/java/Depot.java
@@ -28,7 +28,7 @@ public class Depot {
         return id;
     }
 
-    public void setBalance(Balance newBalance) {
+    public void setBalance(Balance newBalance) throws Exception{
         this.balance = newBalance;
     }
 }
